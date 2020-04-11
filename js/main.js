465ef8a4955e8fe6d846422360b9234c71a2488e
Amankaium <kaium@list.ru>
2020-04-11 23:59:22 +0600

    modified date on script

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "July 1, 2019 12:00:00",
+	date: "May 5, 2020 12:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
